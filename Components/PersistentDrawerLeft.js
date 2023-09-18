3254fb58d73f9acdc95a2b723f3b80b886b055a6
Anshu <anshuagrawal1901302@gmail.com>
2023-09-18 16:03:32 +0530

    second commit

--- Components/PersistentDrawerLeft.js
+++ Components/PersistentDrawerLeft.js
@@ -30,7 +30,7 @@ export default function PersistentDrawerLeft() {
   const { drawerOpen,setDrawerOpen } =useStateContext();
 
   return (
-    <Box sx={{ display: "flex"}}>
+    <Box sx={{ display: "flex" }}>
       <CssBaseline />
       <AppBar position="fixed" open={drawerOpen}>
         <Navbar />
